e1bc15cafb0bd047da39d895d002e5ef8c63a75a
Mikhail Zametaev <112802721+qwers451@users.noreply.github.com>
2025-08-07 20:02:06 +0400

    add tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
+import './App.css'
+
 function App() {
 
   return (
     <>
-      APP
+      <div className="text-center mt-0 text-2xl text-blue-600 font-bold mt-10">
+        Привет из Tailwind!
+      </div>
     </>
   )
 }
